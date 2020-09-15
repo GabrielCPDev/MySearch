0cfdf786d0145b874b1da22834c21efa75a44443
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2020-09-15 14:58:16 -0300

    projeto criado

---
+++ backend/mySearch/src/test/java/com/yggdrasil/mySearch/MySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yggdrasil.mySearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
